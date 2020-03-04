a655c4fbd8babd2e52e382a312f99172651944ff
Timothy Kroon <timothykroon1@gmail.com>
2020-03-04 15:05:06 +0100

    Uitwerking van de exercise

---
+++ for_klas.php
@@ -0,0 +1,6 @@
+<?php
+$array = array('Sjonnie de Wiel', 'Herman Kaal', 'Henk de Steen', 'Inge Kerkhoven', 'Gert Kruiswijk');
+
+for($i = 0; $i < count($array); $i++) {
+    echo $array[$i] . PHP_EOL;
+}
